524d8440b558e059c3fb6fe68648717b1b9ba40b
RussP <rhperry@ucdavis.edu>
2019-04-09 09:03:46 -0700

    uploaded working set of files, still need toggler

--- static/js/logic2.js
+++ static/js/logic2.js
@@ -3,19 +3,22 @@ mapboxgl.accessToken = API_KEY
 var map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/light-v10',
-  center: [0, 0],
-  zoom: 2
+  center: [0, 30],
+  zoom: 1.3
 });
 
+// year[0] = 1980, year[36] = 2016
 years = []
 for(i = 1980; i < 2017; i++) {
   years.push(i);
 };
 
 function filterBy(year) {
-  var filters = ['==', 'time', year.toString()];
+  // meets syntax requirements for filter in mapbox, converts int year to string
+  // to match the year in under the 'time' key of the geoJSON
+  var filters = ['==', 'time', (1980 + year).toString()];
+  // assign filter to lines Layer
   map.setFilter('disease-lines', filters);
-
   // Set the label to the year
   document.getElementById('year').textContent = years[year];
 }
@@ -24,30 +27,10 @@ map.on('load', function() {
   d3.json('/data', function(err, data) {
       if (err) throw err;
 
-      // data.features = data.features.map(function(d) {
-      //   d.properties.year = d.properties.time.slice(0,4);
-      //   console.log(d.properties.time.slice(0,4));
-      //   return d;
-      // });
-
       map.addSource('dlines', {
         'type': 'geojson',
         data: data
       });
-
-      // layers = [
-      //   {
-      //     "id": "testlayer",
-      //     "type": "line",
-      //     "source": "dlines",
-      //     "paint": {
-      //       "line-color": "red",
-      //       "line-width": 3,
-      //       "line-opacity": 0.1
-      //     }
-      //   }
-      // ]
-      // map.addLayer(layers[0]);
 
       map.addLayer({
         'id': `disease-lines`,
@@ -58,12 +41,12 @@ map.on('load', function() {
             'match',
             ['get', 'name'],
             "Malaria", 'red',
+            "Leprosy", "cyan",
             "Yellow Fever", "yellow",
-            "Leprosy", "blue",
-            '#ccc'
+            'purple'
           ],
           "line-width": 3,
-          "line-opacity": 0.1
+          "line-opacity": 0.5
         }
       });
 
@@ -71,7 +54,9 @@ map.on('load', function() {
       filterBy(36);
       
       document.getElementById('slider').addEventListener('input', function(e) {
+        // parse strings of ints from index page, 0 to 36 
         var year = parseInt(e.target.value, 10);
+        // pass int to filterBy
         filterBy(year);
       });
   });
